ee5ca065cfc361989671a0689825b819a5b2feff
Index <57020946+IndexingGitHub@users.noreply.github.com>
2024-01-26 18:42:58 -0600

    Add Native Code block as an addon

--- public/blocks/advanced_blocks.js
+++ public/blocks/advanced_blocks.js
@@ -29,6 +29,35 @@ Blockly.Lua['advanced_nativeLua'] = function(block) {
   return code;
 };
 
+Blockly.Blocks['advanced_nativeLuaAddon'] = {
+  init: function() {
+    this.jsonInit({
+        "message0": "Native Lua Addon %1",
+
+        "args0": [
+          {
+              "type": "input_value",
+              "name": "NATIVE_CODE",
+              "check": "String"
+            },
+        ],
+
+        "colour": "#4f4f4f",
+
+        }
+
+        );
+  }
+};
+
+Blockly.Lua['advanced_nativeLuaAddon'] = function(block) {
+  let subString = Blockly.Lua.valueToCode(block, 'NATIVE_CODE',Blockly.Lua.ORDER_NONE) || '\'\'';
+
+  let code = subString.replace(/'/g,"") + "\n";
+  
+  return code;
+};
+
 Blockly.Blocks['advanced_comment'] = {
   init: function() {
     this.jsonInit({
